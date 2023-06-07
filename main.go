46f1da5457df1c300edb242767ee4d0b0294f43d
Matthew Straughn <mviper101@hotmail.com>
2023-06-06 20:16:13 -0400

    removed unused routes

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	control "find-a-flight/controller"
-	util "find-a-flight/utils"
 	"log"
 	"os"
 
@@ -19,20 +18,6 @@ func init() {
 
 func main() {
 	router := gin.Default()
-
-	router.GET("/hello", func(c *gin.Context) {
-		start, end := util.Convert_to_iso(4)
-		c.JSON(200, gin.H{
-			"message":      "Hello World",
-			"start_time: ": start,
-			"end_time: ":   end,
-		})
-	})
-
-	router.NoRoute(func(ctx *gin.Context) {
-		ctx.File("./frontend/src/routes/+page.svelte")
-	})
-
 	router.GET("/getFlights", control.Get_flights)
 	router.GET("/getFares", control.Get_fares)
 	router.GET("/getData", control.Populate)
