5a5a7a304e77531a85447678979779baa02b2d80
Matthew Straughn <matthewe99@hotmail.com>
2023-11-25 01:27:32 -0500

    allows go routines to run serially, avoids API crashing.

--- controller/controller.go
+++ controller/controller.go
@@ -183,11 +183,12 @@ func Search(c *gin.Context) {
 
 func Populate(c *gin.Context) {
 	enableCors(c)
-	wg.Add(2)
-	go Get_fares(c)
+	wg.Add(1)
 	go Get_flights(c)
 	wg.Wait()
-
+	wg.Add(1)
+	go Get_fares(c)
+	wg.Wait()
 }
 
 func enableCors(c *gin.Context) {
